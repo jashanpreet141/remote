185d84b855385e088f8a3570e73ce34b2589abc3
jashanpreet141 <businessenterprises141@gmail.com>
2022-05-03 10:39:14 +0530

    initial comit

---
+++ sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+
+int a ,b;
+
+int sum;
+sum =a+b;
+printf("Enter the a number")
+scanf("%d",&a);
+printf("Enter the b number")
+scanf("%d",&b);
+
+printf("The answer is = %d",sun);
+}
+
