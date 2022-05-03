2b5baa47a75cecd7a8b03f049de288fd0ee1b784
jashanpreet141 <businessenterprises141@gmail.com>
2022-05-03 12:47:31 +0530

    goofl.c edited

---
+++ goofl.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+	int a,b;
+	printf("Enter the first number =");
+	scanf("%d",&a);
+	printf("Enter the second number =");
+	scanf("%d",&b);
+
+	int sum
+		sum =a+b;
+	printf("The sum is = %d",sum);
+
+}
